58002dc5c1ce0b83c37bb4fb4a8f52b1eb80252d
K3rill <razdor.2019@mail.ru>
2024-09-24 22:52:19 +0300

    bigbanner-react

---
+++ react-version/src/components/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import Button from "../Button/Button";
+import styles from "./Banner.module.scss";
+import { imagesArr } from "./images";
+export default function Banner({ type }) {
+  const [ibg, setIbg] = useState();
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    function changeIbg() {
+      setIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % imagesArr.length;
+        setIbg(imagesArr[nextIndex].url);
+        return nextIndex;
+      });
+    }
+
+    const intervalId = setInterval(() => {
+      changeIbg();
+    }, 20000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.BigBanner}>
+        <img className={styles["BigBanner__ibg"]} src={ibg} alt="" />
+        <div className={styles["BigBanner__main-content"]}>
+          <h1 className={styles["BigBanner__title"]}>Spirited Away</h1>
+          <div className={styles["BigBanner__buttons"]}>
+            <Button buttonType={"white"}>Watch</Button>
+            <Button buttonType={"black"}>About</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ react-version/src/components/Banner/images.js
@@ -0,0 +1,18 @@
+export const imagesArr = [
+  {
+    title: "red-sunrise",
+    url: "https://avatars.mds.yandex.net/i?id=d79ef4da83f4375f998e41fe4605dc65_l-7756406-images-thumbs&n=13",
+  },
+  {
+    title: "sunset-in-forest",
+    url: "https://avatars.mds.yandex.net/i?id=47003c56ba0a8eb359f13dec653b4298_l-9243216-images-thumbs&n=13",
+  },
+  {
+    title: "girl-and-sunset",
+    url: "https://img.goodfon.ru/original/2880x1800/a/18/devushka-zakat-gorod-2.jpg",
+  },
+  {
+    title: "anime-landscape",
+    url: "https://i1.wp.com/www.teahub.io/photos/full/304-3049893_anime-landscape-sunset-plants-field-sky-anime-red.jpg",
+  },
+];
