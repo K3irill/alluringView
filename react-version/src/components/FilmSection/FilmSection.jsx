bedfe1b15b477cdf34964f9feacc04aa78fd6a28
Kirill <razdor.2019@mail.ru>
2024-10-06 15:10:06 +0300

    breakpoint for slider

--- react-version/src/components/FilmSection/FilmSection.jsx
+++ react-version/src/components/FilmSection/FilmSection.jsx
@@ -5,23 +5,46 @@ import "slick-carousel/slick/slick-theme.css";
 import { useFetchFilms } from "../../hooks/useFetchFilms";
 
 export function FilmSection({ apiUrl, TitleCollection, onFilmSelect }) {
-  const { films, error} = useFetchFilms(apiUrl)
+  const { films, error } = useFetchFilms(apiUrl);
 
   if (error) {
     return <div>Error: {error}</div>;
   }
 
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div className={styles["FilmSection"]}>
-        <h2>{TitleCollection}</h2>
+      <h2>{TitleCollection}</h2>
       <div className={styles["FilmSection__cards-wrapper"]}>
         <Slider {...settings}>
           {films.map((film) => {
